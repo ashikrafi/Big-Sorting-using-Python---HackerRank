86f0e59f06a3b7894b6369e54ae48fcab24c046d
ashikrafi <ashik.rafi@hotmail.com>
2020-01-26 23:43:43 +0600

    Add files via upload

---
+++ Big Query.py
@@ -0,0 +1,6 @@
+echo "# Big-Sorting-using-Python---HackerRank" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/ashikrafi/Big-Sorting-using-Python---HackerRank.git
+git push -u origin master
